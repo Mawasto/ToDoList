d261a331cc1f09b91eaae8da6d4dca02067c7cc6
Mawasto <mitana.kacper1@gmail.com>
2025-02-13 22:09:20 +0100

    Installed CommunityToolkit.MVVM Added NotifyPropertyChanged, and ToDoItem model.

--- ToDoList1/Models/ToDoItem.cs
+++ ToDoList1/Models/ToDoItem.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ToDoList1.Models
 {
-    internal class ToDoItem
+    public partial class TodoItem : NotifyPropertyChanged
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        private bool _isCompleted;
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            set
+            {
+                _isCompleted = value;
+                OnPropertyChanged();
+            }
+
+        }
     }
 }
